a741b942131697bacd78f0db1163ac31904e71be
MQuero <manuelquerocar@hotmail.com>
2023-09-11 23:10:25 +0200

    20230911
    Export proyect to platformio.
    Add release 1.1 of omniLidarWeb.

---
+++ include/main.h
@@ -0,0 +1,416 @@
+#define SERIALVEL 115200
+
+/* I2C PINS
+* I2C0 --> VL53L1X
+* I2C1 --> AS5600
+*/
+#define SDA0 19
+#define SCL0 18
+#define SDA1 23
+#define SCL1 5
+#define I2CSPEED 400000u
+
+/* 
+* VL53L1X Pins
+*/
+#define IRQ_PIN 15
+#define XSHUT_PIN 13
+
+/*
+* MOTOR PARAMETERS
+*/
+#define PSVOLTAGE 14.8f //nominal voltage of the battery
+#define MOTORVOLLIMIT 17
+#define MOTORVELLIMIT 20
+#define PIDVELP 0.05f
+#define PIDVELI 1
+#define PIDPOSP 20
+#define TFLPFVEL 0.01f
+#define INITIALMOTPOST 0.0f
+
+/*
+* Defaults Values
+*/
+#define DEFLIDARMAXDIST 2000 // Max meas distance of the sensor
+#define DEFLIDARMEASPERREV 10 // Number Meas of each revolution
+#define DEFLIDARTIMEOFREV 2000 // in ms
+#define DEFLIDARMEASTIME 50 // Valid timing budgets: 15, 20, 33, 50, 100, 200 and 500ms
+
+/*
+ * Web Server Parameters
+ */
+const char *host = "omnilidar";
+const char *ssid = "TP-LINK_26619E";
+const char *password = "18670691";
+
+void motorHoming();
+void normalOperation();
+void sendData();
+void stopMotor();
+
+
+const char *htmlContent = R"(
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>omniLidar Web Control</title>
+    <style>
+        h1 {
+          color: green;
+          font-size: 40px;
+        }
+        body {
+            background-color: #FFD700;
+            font-family: Arial, sans-serif;
+            margin: 0;
+            display: flex;
+            text-align: center;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+
+        .container {
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            gap: 10px;
+            padding: 20px;
+        }
+
+        .label {
+            padding-left: 0px;
+            font-size: 20px;
+        }
+
+        .input {
+            padding: 5px;
+            width: 30%;
+        }
+
+        .center-button {
+            margin-top: 20px;
+            background-color: #cc9900;
+            color: white;
+            border: 2px solid black;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            border-radius: 0;
+            cursor: pointer;
+        }
+
+        .center-button[disabled] {
+            background-color: #ffd966;
+            cursor: not-allowed; /* optional: change the cursor to indicate the button is disabled */
+        }
+
+
+        .start-button {
+            margin-right: 10px;
+            background-color: #339933;
+            color: white;
+            border: 2px solid black;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            cursor: pointer;
+        }
+
+        .start-button[disabled] {
+            background-color: #8cd98c;
+            cursor: not-allowed; /* optional: change the cursor to indicate the button is disabled */
+        }
+
+        .stop-button {
+            margin-left: 10px;
+            background-color: #cc3300;
+            border: 2px solid black;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            cursor: pointer;
+        }
+
+        .stop-button[disabled] {
+            background-color: #ff8c66;
+            cursor: not-allowed; /* optional: change the cursor to indicate the button is disabled */
+        }
+
+        .update-button {
+            margin-left: 10px;
+            background-color: #00ccff;
+            border: 2px solid black;
+            color: black;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            cursor: pointer;
+        }
+
+        .canvas-terminal-container {
+            display: flex;
+            margin-top: 20px;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        #graphCanvas {
+            max-width: 90%;
+            height: auto;
+            margin-top: 20px;
+            border: 2px solid red;
+        }
+
+        #dataTerminal {
+            overflow-y: scroll;
+            height: 500px;
+            width: 300px;
+            margin-left: 40px;
+            border: 2px solid black;
+            background-color: #ffd966;
+            color: black;
+            margin-top: 20px;
+            font-family: monospace;
+            font-size: 15px;
+        }
+
+    </style>
+</head>
+
+<body>
+    <h1>omniLidar Web Control</h1>
+    <form method="POST" action="/">
+        <div class="container">
+            <div>
+                <label class="label" for="lidarTime">Lidar - Time of measure [ms]:</label>
+                <input class="input" type="number" id="lidarTime" name="lidarTime">
+            </div>
+
+            <div>
+                <label class="label" for="measTimePerRev [ms]">Meas - Time of Revolution:</label>
+                <input class="input" type="number" id="measTimePerRev" name="measTimePerRev">
+            </div>
+        </div>
+        <div class="container">
+            <div>
+                <label class="label" for="lidarMaxDistance">Lidar - Max meas distance [ms]:</label>
+                <input class="input" type="number" id="lidarMaxDistance" name="lidarMaxDistance">
+            </div>
+            <div>
+                <label class="label" for="measNumPerRev">Meas - Num per revolution:</label>
+                <input class="input" type="number" id="measNumPerRev" name="measNumPerRev">
+            </div>
+        </div>
+        <button type="button" class="center-button start-button" id="startBtn">Start</button>
+        <button type="submit" class="center-button">Save</button>
+        <button type="button" class="center-button stop-button" id="stopBtn" disabled>Stop</button>
+    </form>
+
+    <div class="canvas-terminal-container">
+        <canvas id='graphCanvas' width='500' height='500'></canvas>
+        <div id="dataTerminal"></div>
+    </div>
+
+    <form action = "/serverIndex" method = "GET">
+        <button type="submit" class="center-button update-button">Update Firmware</button>
+    </form>
+
+    <script>
+
+        var lidarMaxDistance = 2000;  // Initial default value
+
+        var canvas = document.getElementById('graphCanvas');
+        var ctx = canvas.getContext('2d');
+
+        function drawTemplate(){
+
+            ctx.fillStyle = 'lightgray';
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            ctx.fillStyle = 'blue';
+
+            // Draw coordinate axis and circles
+            var centerX = canvas.width / 2;
+            var centerY = canvas.height / 2;
+            var radius = Math.min(centerX, centerY) - 20;
+
+            // Draw coordinate axis
+            ctx.strokeStyle = 'black';
+            ctx.beginPath();
+            ctx.moveTo(centerX, 0);
+            ctx.lineTo(centerX, canvas.height);
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.moveTo(0, centerY);
+            ctx.lineTo(canvas.width, centerY);
+            ctx.stroke();
+
+            // Draw lines at 45 degrees
+            ctx.strokeStyle = 'green';
+            ctx.beginPath();
+            ctx.moveTo(centerX - radius, centerY + radius);
+            ctx.lineTo(centerX + radius, centerY - radius);
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.moveTo(centerX - radius, centerY - radius);
+            ctx.lineTo(centerX + radius, centerY + radius);
+            ctx.stroke();
+
+
+            // Draw circles representing radians
+            ctx.strokeStyle = 'gray';
+            var distanceBetweenCircles = lidarMaxDistance / 5;
+            var textdistance = 0;
+            for (var i = 1; i <= 5; i++) {
+                ctx.beginPath();
+                ctx.arc(centerX, centerY, radius * (i / 5), 0, 2 * Math.PI);
+                ctx.stroke();
+                ctx.fillText(Math.round(textdistance) + "m", centerX + (radius - 20) * (i / 5), centerY-5);
+                textdistance += distanceBetweenCircles;
+            }
+        }
+        
+
+        function clearCanvas(){
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+        }
+
+        function appendToTerminal(data) {
+            const terminal = document.getElementById("dataTerminal");
+            const entry = document.createElement("div");
+            entry.textContent = data;
+            terminal.appendChild(entry);
+            terminal.scrollTop = terminal.scrollHeight; // Scroll to bottom
+        }
+
+        function drawPoints(points) {
+            clearCanvas();
+            drawTemplate();
+            
+            const scaleFactor = canvas.width / (2*lidarMaxDistance);
+
+            appendToTerminal("New Data:");
+            for(let i = 0; i < points.length; i++) {
+                const x = (points[i][0] * Math.cos(points[i][1])) * scaleFactor + canvas.width / 2; 
+                const y = (points[i][0] * Math.sin(points[i][1])) * scaleFactor + canvas.height / 2;
+
+                ctx.beginPath();
+                ctx.arc(x, y, 5, 0, 5 * Math.PI); 
+                ctx.fillStyle = 'red'; 
+                ctx.fill();
+                appendToTerminal(`Dis: ${points[i][0].toFixed(2)}, Ang: ${points[i][1].toFixed(2)}`);
+            }
+        }
+
+        var socket = new WebSocket('ws://' + location.hostname + ':81/');
+        socket.onmessage = function(event) {
+            const data = event.data;
+            
+            if (data.startsWith("lidarMaxDistanceUpdate:")) {
+                // Extract and update the lidarMaxDistance
+                lidarMaxDistance = parseInt(data.split(":")[1], 10);
+                drawTemplate();  // Redraw the template with updated value
+            } else if (data === "isStopped") { // if isStopped is true
+                document.querySelector(".center-button[type='submit']").disabled = false;
+                document.querySelector(".stop-button[type='button']").disabled = true;
+                document.querySelector(".start-button[type='button']").disabled = false;
+            } else if (data === "isStarted") {
+                document.querySelector(".center-button[type='submit']").disabled = true;
+                document.querySelector(".stop-button[type='button']").disabled = false;
+                document.querySelector(".start-button[type='button']").disabled = true;
+                
+            } else if (data !== "noCommand") {
+                const pointStrings = data.split("|");
+                const pointArray = pointStrings.map(str => {
+                  const coords = str.split(",");
+                  return [parseFloat(coords[0]), parseFloat(coords[1])];
+                });
+                drawPoints(pointArray);
+            }
+        }
+
+        document.getElementById('startBtn').addEventListener('click', function() {
+            socket.send('start');
+        });
+
+        document.getElementById('stopBtn').addEventListener('click', function() {
+            socket.send('stop');
+        });
+
+        // Handle form submission
+        document.querySelector("form").addEventListener('submit', function(event) {
+            event.preventDefault();  // prevent default form submission
+
+            // Construct the form data object
+            var formData = {
+                lidarTime: document.getElementById("lidarTime").value,
+                measTimePerRev: document.getElementById("measTimePerRev").value,
+                lidarMaxDistance: document.getElementById("lidarMaxDistance").value,
+                measNumPerRev: document.getElementById("measNumPerRev").value
+            };
+
+            // Send the form data as a JSON string through the WebSocket
+            socket.send(JSON.stringify(formData));
+        });
+    </script>
+</body>
+
+</html>
+)";
+
+/*
+ * Server Index Page
+ */
+const char *serverIndex = R"(
+    <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+
+    <form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>
+        <input type='file' name='update'>
+        <input type='submit' value='Update'>
+    </form>
+
+    <div id='prg'>progress: 0%</div>
+
+    <script>
+        $('form').submit(function(e){
+            e.preventDefault();
+            var form = $('#upload_form')[0];
+            var data = new FormData(form);
+            $.ajax({
+              url: '/update',
+              type: 'POST',
+              data: data,
+              contentType: false,
+              processData:false,
+              xhr: function() {
+                  var xhr = new window.XMLHttpRequest();
+                  xhr.upload.addEventListener('progress', function(evt) {
+                      if (evt.lengthComputable) {
+                          var per = evt.loaded / evt.total;
+                          $('#prg').html('progress: ' + Math.round(per*100) + '%');
+                      }
+                  }, false);
+                  return xhr;
+              },
+              success:function(d, s) {
+                  console.log('success!')
+              },
+              error: function (a, b, c) {}
+            });
+        });
+    </script>
+)";
---
+++ src/main.cpp
@@ -0,0 +1,393 @@
+#include <WebServer.h>
+#include <WebSocketsServer.h>
+#include <ArduinoJson.h>
+
+#include <ESPmDNS.h>
+#include <ArduinoOTA.h>
+#include <Update.h>
+
+#include <SimpleFOC.h>
+#include <VL53L1X.h>
+
+#include <math.h>
+#include <vector>
+#include "main.h"
+
+WebServer server(80);
+WebSocketsServer webSocket(81); // create a websocket server on port 81
+/*********************************************/
+
+/*
+ * Variables Declarations
+ */
+int lidarMaxDist = DEFLIDARMAXDIST, lidarMeasperRev = DEFLIDARMEASPERREV, lidarTimeofRev = DEFLIDARTIMEOFREV, lidarMeasTime = DEFLIDARMEASTIME;
+float lidarOffsetbwMeas = (2 * PI) / lidarMeasperRev;
+float lidarTimeofMeas = lidarTimeofRev / lidarMeasperRev;
+
+float motorPosition = INITIALMOTPOST;
+
+// Vector arrays to store data
+std::vector<std::vector<float>> coordsPolar;
+std::vector<std::vector<float>> coordsCartesian;
+
+// Variables to store the value from webserver
+int toPutLidarMaxDist, toPutlidarMeasperRev, toPutlidarTimeofRev, toPutlidarMeasTime;
+bool isPutNewValues = false, isStarted = false;
+
+/*
+ * 0 --> First Run - Motor Homing
+ * 1 --> Normal Operation
+ * 2 --> Send Data
+ * 3 --> Stop Motor
+ */
+uint8_t state = 0;
+
+uint16_t niteration = 0;
+unsigned long previousUpdateTime = 0;
+/*********************************************/
+
+/*  I2C Objects Declaration  */
+TwoWire I2CZero = TwoWire(0);
+TwoWire I2COne = TwoWire(1);
+
+/*  VL53L1X Object Declaration  */
+VL53L1X VL53L1Xsensor;
+
+/*  Motor Objects Declarations */
+MagneticSensorI2C AS5600sensor = MagneticSensorI2C(AS5600_I2C); // Encoder of the motor
+BLDCMotor motor = BLDCMotor(7);                                 // Number of poles
+BLDCDriver3PWM motorDriver = BLDCDriver3PWM(26, 27, 14, 12);
+
+void handleRoot()
+{
+  server.send(200, "text/html", htmlContent);
+}
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
+{
+  switch (type)
+  {
+  case WStype_DISCONNECTED:
+    Serial.printf("[%u] Disconnected!\n", num);
+    break;
+  case WStype_CONNECTED:
+  {
+    IPAddress ip = webSocket.remoteIP(num);
+    Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+  }
+  break;
+  case WStype_TEXT:
+
+    if (length > 0 && payload[0] == '{')
+    { // Check if payload is a JSON string (basic check)
+      // Parse JSON payload for form data
+      DynamicJsonDocument doc(1024);
+      deserializeJson(doc, (char *)payload);
+      toPutlidarMeasTime = doc["lidarTime"];
+      toPutlidarTimeofRev = doc["measTimePerRev"];
+      toPutLidarMaxDist = doc["lidarMaxDistance"];
+      toPutlidarMeasperRev = doc["measNumPerRev"];
+      // Print the values (you can remove this if not needed)
+      Serial.println("Values saved via WebSocket:");
+      Serial.print("Lidar Time: ");
+      Serial.println(toPutlidarMeasTime);
+      Serial.print("Meas Time per Revolution: ");
+      Serial.println(toPutlidarTimeofRev);
+      Serial.print("Lidar Max Meas Distance: ");
+      Serial.println(toPutLidarMaxDist);
+      Serial.print("Meas Num per Revolution: ");
+      Serial.println(toPutlidarMeasperRev);
+
+      // Send updated lidarMaxDistance to frontend
+      String lidarMaxDistanceUpdate = "lidarMaxDistanceUpdate:" + String(toPutLidarMaxDist);
+      webSocket.broadcastTXT(lidarMaxDistanceUpdate.c_str());
+      return;
+    }
+
+    String msg = String((char *)payload);
+    if (msg == "start")
+    {
+      Serial.println("Start Button");
+      isStarted = true;
+      webSocket.broadcastTXT("isStarted");
+    }
+    else if (msg == "stop")
+    {
+      Serial.println("Stop Button");
+      isStarted = false;
+      webSocket.broadcastTXT("isStopped");
+    }
+    break;
+  }
+}
+
+void setup()
+{
+  /*
+   * Serial Init and configuration
+   */
+  Serial.begin(SERIALVEL);
+  delay(1000);
+  Serial.print(F("Serial Velocity: "));
+  Serial.println(SERIALVEL);
+  /*********************************************/
+
+  /*
+   * WiFi Setup
+   */
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+  Serial.print("IP Address ");
+  Serial.println(WiFi.localIP());
+
+  //  Use mdns for host name resolution
+  if (!MDNS.begin(host)) // http://omniLidar.local
+  {
+    Serial.println("Error setting up MDNS responder!");
+    delay(5000);
+    Serial.println("mDNS responder started");
+  }
+
+  server.on("/", handleRoot); // Start Main Page
+
+  // OTA Conf
+  server.on("/serverIndex", HTTP_GET, []()
+            {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", serverIndex); });
+
+  /*handling uploading firmware file */
+  server.on(
+      "/update", HTTP_POST, []()
+      {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+    ESP.restart(); },
+      []()
+      {
+        HTTPUpload &upload = server.upload();
+        if (upload.status == UPLOAD_FILE_START)
+        {
+          Serial.printf("Update: %s\n", upload.filename.c_str());
+          if (!Update.begin(UPDATE_SIZE_UNKNOWN))
+          { // start with max available size
+            Update.printError(Serial);
+          }
+        }
+        else if (upload.status == UPLOAD_FILE_WRITE)
+        {
+          /* flashing firmware to ESP*/
+          if (Update.write(upload.buf, upload.currentSize) != upload.currentSize)
+          {
+            Update.printError(Serial);
+          }
+        }
+        else if (upload.status == UPLOAD_FILE_END)
+        {
+          if (Update.end(true))
+          { // true to set the size to the current progress
+            Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+          }
+          else
+          {
+            Update.printError(Serial);
+          }
+        }
+      });
+
+  server.begin();
+  Serial.println("HTTP server started");
+
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+  Serial.println("WebSocket server started");
+  /*********************************************/
+
+  /*
+   * I2C Setups
+   */
+  if (!I2CZero.begin(SDA0, SCL0, I2CSPEED))
+  {
+    Serial.println(F("I2C 0 - NOT initiated."));
+  }
+  Serial.println(F("I2C 0 - successfully initiated."));
+
+  if (!I2COne.begin(SDA1, SCL1, I2CSPEED))
+  {
+    Serial.println(F("I2C 1 - NOT initiated."));
+  }
+  Serial.println(F("I2C 1 - successfully initiated."));
+  /*********************************************/
+
+  /*
+   * Motor Setups
+   */
+  AS5600sensor.init(&I2COne);
+  Serial.println(F("Motor - Encoder initiated."));
+  motor.linkSensor(&AS5600sensor);
+  Serial.println(F("Motor - Encoder linked to motor."));
+
+  // Motor driver conf
+  motorDriver.voltage_power_supply = PSVOLTAGE;
+  Serial.print(F("Motor - Power supply voltage: "));
+  Serial.println(PSVOLTAGE);
+  motorDriver.init();
+
+  // Motor conf
+  motor.linkDriver(&motorDriver);
+  motor.foc_modulation = FOCModulationType::SpaceVectorPWM;
+  Serial.println(F("Motor - Modulation Type: Space Vector PWM."));
+  motor.controller = MotionControlType::angle;
+  Serial.println(F("Motor - Control Type: angle/position."));
+  motor.PID_velocity.P = PIDVELP;
+  motor.PID_velocity.I = PIDVELI;
+  motor.P_angle.P = PIDPOSP;
+  motor.LPF_velocity.Tf = TFLPFVEL;
+  Serial.println(F("Motor - PID Values:"));
+  Serial.print(F("Motor - Velocity P: "));
+  Serial.println(PIDVELP);
+  Serial.print(F("Motor - Velocity I: "));
+  Serial.println(PIDVELI);
+  Serial.print(F("Motor - Position P: "));
+  Serial.println(PIDPOSP);
+  Serial.print(F("Motor - Time constant LPF: "));
+  Serial.println(TFLPFVEL);
+  motor.voltage_limit = MOTORVOLLIMIT;
+  Serial.print(F("Motor - Voltage Limit: "));
+  Serial.println(MOTORVOLLIMIT);
+  motor.velocity_limit = MOTORVELLIMIT;
+  Serial.print(F("Motor - Velocity Limit: "));
+  Serial.println(MOTORVELLIMIT);
+  motor.useMonitoring(Serial);
+
+  // Init Motor and FOC
+  motor.init();
+  motor.initFOC();
+  /*********************************************/
+
+  /*
+   * VL53L1X Setup
+   */
+  // Initialize the xshut pin and power on the VL53L1X
+  pinMode(XSHUT_PIN, OUTPUT);
+  digitalWrite(XSHUT_PIN, LOW);
+  delay(10); // Wait for a short duration before enabling the sensor
+  digitalWrite(XSHUT_PIN, HIGH);
+  delay(10); // Wait for sensor to power up
+
+  VL53L1Xsensor.setBus(&I2CZero);
+  // Initialize the VL53L1X sensor
+  if (!VL53L1Xsensor.init())
+  {
+    Serial.println("Failed to detect and initialize sensor!");
+    while (1)
+      ;
+  }
+  // Start continuous measurements
+  VL53L1Xsensor.startContinuous(lidarMeasTime);
+
+  /*
+   * Init main program
+   */
+  Serial.println(F("State 0 - First Run Detected"));
+  Serial.println(F("Motor - Homing"));
+}
+
+void loop()
+{
+  server.handleClient();
+  ArduinoOTA.handle();
+  webSocket.loop();
+  motor.loopFOC();
+  motor.move(motorPosition);
+
+  switch (state)
+  {
+  case 0:
+    motorHoming();
+    break;
+  case 1:
+    normalOperation();
+    break;
+  case 2:
+    // sendData();
+    break;
+  case 3:
+    // stopMotor();
+    break;
+
+  default:
+    break;
+  }
+
+  // static unsigned long lastTime = 0;
+  // if (millis() - lastTime > 1000)
+  // {
+  //   lastTime = millis();
+
+  //   float n = 0.0;
+  //   String dataToSend = "";
+  //   for (int i = 0; i < maxPoints; i++)
+  //   {
+  //     points[i][0] = random(0, toPutLidarMaxDistance);    // Distance
+  //     points[i][1] = n;                                   // angle
+  //     pointsMod[i][0] = points[i][0] * cos(points[i][1]); // x points
+  //     pointsMod[i][1] = points[i][0] * sin(points[i][1]); // y points
+
+  //     if (i > 0)
+  //     {
+  //       dataToSend += "|";
+  //     }
+  //     dataToSend += String(points[i][0]) + "," + String(points[i][1]);
+  //     n = n + (2 * PI / maxPoints);
+  //   }
+  //   webSocket.broadcastTXT(dataToSend);
+  // }
+}
+
+void motorHoming()
+{
+  if (AS5600sensor.getAngle() < (INITIALMOTPOST + 0.02) || AS5600sensor.getAngle() < fabs(INITIALMOTPOST - 0.02))
+  {
+    Serial.println(F("Motor - Home Position"));
+    state = 1;
+    Serial.println(F("State 1 - Normal Operation"));
+  }
+}
+
+void normalOperation()
+{
+  if ((millis() - previousUpdateTime) > lidarTimeofMeas)
+  {
+    previousUpdateTime = millis();
+    motorPosition += lidarOffsetbwMeas;
+
+    Serial.print(F("Numero de iteracion: "));
+    Serial.println(niteration);
+
+    uint16_t distance = VL53L1Xsensor.read();
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(" mm");
+
+    coordsPolar[niteration][0] = distance;
+    coordsPolar[niteration][1] = AS5600sensor.getAngle();
+
+    if (niteration == (lidarMeasperRev - 1))
+    {
+      Serial.println(F("Data - Data ready"));
+      state = 2;
+      Serial.println(F("State 2 - Send Data"));
+      niteration = 0;
+    }
+    else
+    {
+      niteration += 1;
+    }
+  }
+}
